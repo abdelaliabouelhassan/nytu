27b1f4f99e96c1b02a8b735649712f6f31b2b836
abdelali abouelhassan <abdelaliabouelhassan@gmail.com>
2023-06-15 18:47:41 +0100

    references first section

--- pages/references/index.vue
+++ pages/references/index.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class=" text-black">
-       comming soon...
+    <div class=" w-full">
+        <FirstSection />
     </div> 
 </template>
 
 
 
 <script setup>
+ import FirstSection from "@/components/partials/sections/references/FirstSection.vue"
+
  useSeoMeta({ // change title and description values to your needs
     title: 'Nytu - References',
     description: 'Learn more about the events you like in your location.',
