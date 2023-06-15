955232850babbd8fa33555bdaa8ced70f4dee200
abdelali abouelhassan <abdelaliabouelhassan@gmail.com>
2023-06-15 21:04:38 +0100

    demo-planen page

--- pages/demo-planen/index.vue
+++ pages/demo-planen/index.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class=" text-black">
-       comming soon...
+    <div class=" w-full">
+       <FirstSection />
     </div> 
 </template>
 
 
 
 <script setup>
+ import FirstSection from "@/components/partials/sections/demoplanen/FirstSection.vue"
+ 
  useSeoMeta({ // change title and description values to your needs
     title: 'Nytu - Demo Planen',
     description: 'Learn more about the events you like in your location.',
