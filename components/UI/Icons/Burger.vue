596a6bab1bac1de205b98bf6fbefdeec5e8f10af
abdelali abouelhassan <abdelaliabouelhassan@gmail.com>
2023-06-24 00:40:30 +0100

    header & nav

---
+++ components/UI/Icons/Burger.vue
@@ -0,0 +1,14 @@
+<template>
+     <svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_1102_118)">
+                <line x1="0.728027" y1="3" x2="22.728" y2="3" stroke="#9099FF" stroke-width="2"/>
+                <line x1="0.728027" y1="11" x2="22.728" y2="11" stroke="#9099FF" stroke-width="2"/>
+                <line x1="0.728027" y1="18" x2="22.728" y2="19" stroke="#9099FF" stroke-width="2"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1102_118">
+                <rect width="22" height="20" fill="white" transform="translate(0.728027)"/>
+                </clipPath>
+                </defs>
+                </svg>
+</template>
