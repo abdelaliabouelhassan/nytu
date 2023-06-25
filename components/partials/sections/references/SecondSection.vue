c72f21bab6d197f3d76e050297bbaf7c798d68a8
abdelali abouelhassan <abdelaliabouelhassan@gmail.com>
2023-06-25 02:04:22 +0100

    references

--- components/partials/sections/references/SecondSection.vue
+++ components/partials/sections/references/SecondSection.vue
@@ -1,12 +1,12 @@
 <template>
     <section class=" w-full main-container  pt-10 pb-28">
-        <div class=" w-full grid grid-cols-3 gap-x-8 gap-y-20">
+        <div class=" w-full grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-20">
             <EventCard v-for="(event,index) in events" :key="index" :event="event" />
             <EventCard v-for="(event,index) in events" :key="index" :event="event" />
             <EventCard v-for="(event,index) in events" :key="index" :event="event" />
         </div>
         <div class="flex flex-col items-center text-primary gap-y-6 pt-9 cursor-pointer">
-            <div class="text-lg font-bold">Mehr anzeigen</div>
+            <div class=" text-base md:text-lg font-bold">Mehr anzeigen</div>
             <svg class="h-4 w-9" width="38" height="22" viewBox="0 0 38 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M36.5 2.4397L19 19.9397L1.5 2.4397" stroke="#9099FF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
